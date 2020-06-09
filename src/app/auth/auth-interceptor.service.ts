7c3c24238ba7a383db74096a8f2053dc00470ed5
vishal <vishal.verma12914@gmail.com>
2020-06-09 11:59:06 +0530

    Dispatching Auth Actions and follow some general practice

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,19 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpParams } from '@angular/common/http';
 import { AuthService } from './auth.service';
-import { take, exhaustMap } from 'rxjs/operators';
+import { take, exhaustMap, map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+
 //This service is used for modified all url to add token in all url
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
     //Modified the incoming req: add user token in cuurent req and then return modified req...
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         //pipe is used to add two subscriber like user and next.hanlde subs here...
         //take will do automatically unsubscibe
         //exhaustMap is used to map two obserable user and get ob.1st user obser do its work then after taking data from that get obser will be replaced with that
-        return this.authService.user.pipe(
+        return this.store.select('auth').pipe(
             take(1), 
+            map(authState => {
+                return authState.user;
+            }),
             exhaustMap(user => {      
                 if(!user) {
                     return next.handle(req);
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -3,16 +3,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { map, take } from 'rxjs/operators';
+import * as fromApp from '../store/app.reducer';
+import { Store } from '@ngrx/store';
 
 //This service is used to avoid the access to page by direct url
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService, private router: Router) {}
+    constructor(private authService: AuthService, private router: Router, private store: Store<fromApp.AppState>) {}
 
     canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): boolean | UrlTree | Promise<boolean | UrlTree> |  Observable<boolean | UrlTree> {
-        return this.authService.user.pipe(
+        return this.store.select('auth').pipe(
             take(1),
+            map(authState => {
+                return authState.user
+            }),
             map(user => {
                 const isAuth = !!user;
                 if(isAuth) {
